5484f7ee0ffb7734d9e2271632b7d6ea6b60f513
Nathan Florencio <nflorencio13@gmail.com>
2024-03-10 00:05:50 -0300

    validate env variables.

--- src/components/checkout/step-finish.tsx
+++ src/components/checkout/step-finish.tsx
@@ -3,13 +3,14 @@ import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import { generateMessage } from '@/lib/generate-message'
 import { useCartStore } from '@/stores/cart-store'
+import { env } from '@/lib/env'
 
 export const StepFinish = () => {
   const { name, address } = useCheckoutStore((state) => state)
   const { cart } = useCartStore((state) => state)
 
   const message = generateMessage(name, address, cart)
-  const linkZap = `https://wa.me//${process.env.NEXT_PUBLIC_ZAP}?text=${encodeURI(message)}`
+  const linkZap = `https://wa.me//${env.NEXT_PUBLIC_ZAP}?text=${encodeURI(message)}`
 
   return (
     <div className="text-center flex flex-col gap-5">
